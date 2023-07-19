f144760085b197a319898b316a127be1644dbc05
Simon Vieille <simon.vieille@trinaps.com>
2023-07-19 20:19:16 +0200

    add boolean field for CRUD

---
+++ src/core/Crud/Field/BooleanField.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Core\Crud\Field;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * class BooleanField.
+ *
+ * @author Simon Vieille <simon@deblan.fr>
+ */
+class BooleanField extends Field
+{
+    public function configureOptions(OptionsResolver $resolver): OptionsResolver
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            'view' => '@Core/admin/crud/field/boolean.html.twig',
+        ]);
+
+        return $resolver;
+    }
+}
