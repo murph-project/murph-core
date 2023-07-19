f1d956ee5c54f8cba16c91303ef002e3cdfebb4a
Simon Vieille <simon.vieille@trinaps.com>
2023-07-19 20:20:55 +0200

    add context variable in each controllers to simplify overrides

--- src/core/Resources/maker/controller/CrudController.tpl.php
+++ src/core/Resources/maker/controller/CrudController.tpl.php
@@ -15,58 +15,65 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class <?= $class_name; ?> extends CrudController
 {
-    #[Route(path: "/admin/<?= $route; ?>/{page}", name: "admin_<?= $route; ?>_index", methods: ['GET'], requirements: ['page' => '\d+'])]
+    protected ?CrudConfiguration $configuration = null;
+
+    #[Route(path: '/admin/<?= $route; ?>/{page}', name: 'admin_<?= $route; ?>_index', methods: ['GET'], requirements: ['page' => '\d+'])]
     public function index(RepositoryQuery $query, Request $request, Session $session, int $page = 1): Response
     {
         return $this->doIndex($page, $query, $request, $session);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/new", name: "admin_<?= $route; ?>_new", methods: ['GET', 'POST'])]
+    #[Route(path: '/admin/<?= $route; ?>/new', name: 'admin_<?= $route; ?>_new', methods: ['GET', 'POST'])]
     public function new(Factory $factory, EntityManager $entityManager, Request $request): Response
     {
         return $this->doNew($factory->create(), $entityManager, $request);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/show/{entity}", name: "admin_<?= $route; ?>_show", methods: ['GET'])]
+    #[Route(path: '/admin/<?= $route; ?>/show/{entity}', name: 'admin_<?= $route; ?>_show', methods: ['GET'])]
+    #[IsGranted('show', 'entity')]
     public function show(Entity $entity): Response
     {
         return $this->doShow($entity);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/filter", name: "admin_<?= $route; ?>_filter", methods: ['GET'])]
+    #[Route(path: '/admin/<?= $route; ?>/filter', name: 'admin_<?= $route; ?>_filter', methods: ['GET'])]
     public function filter(Session $session): Response
     {
         return $this->doFilter($session);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/edit/{entity}", name: "admin_<?= $route; ?>_edit", methods: ['GET', 'POST'])]
+    #[Route(path: '/admin/<?= $route; ?>/edit/{entity}', name: 'admin_<?= $route; ?>_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('edit', 'entity')]
     public function edit(Entity $entity, EntityManager $entityManager, Request $request): Response
     {
         return $this->doEdit($entity, $entityManager, $request);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/inline_edit/{entity}/{context}/{label}", name: 'admin_<?= $route; ?>_inline_edit', methods: ['GET', 'POST'])]
+    #[Route(path: '/admin/<?= $route; ?>/inline_edit/{entity}/{context}/{label}', name: 'admin_<?= $route; ?>_inline_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('edit', 'entity')]
     public function inlineEdit(string $context, string $label, Entity $entity, EntityManager $entityManager, Request $request): Response
     {
         return $this->doInlineEdit($context, $label, $entity, $entityManager, $request);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/sort/{page}", name: "admin_<?= $route; ?>_sort", methods: ['POST'], requirements: ['page' => '\d+'])]
+    #[Route(path: '/admin/<?= $route; ?>/sort/{page}', name: 'admin_<?= $route; ?>_sort', methods: ['POST'], requirements: ['page' => '\d+'])]
     public function sort(RepositoryQuery $query, EntityManager $entityManager, Request $request, Session $session, int $page = 1): Response
     {
         return $this->doSort($page, $query, $entityManager, $request, $session);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/batch/{page}", name: "admin_<?= $route; ?>_batch", methods: ['POST'], requirements: ['page' => '\d+'])]
+    #[Route(path: '/admin/<?= $route; ?>/batch/{page}', name: 'admin_<?= $route; ?>_batch', methods: ['POST'], requirements: ['page' => '\d+'])]
     public function batch(RepositoryQuery $query, EntityManager $entityManager, Request $request, Session $session, int $page = 1): Response
     {
         return $this->doBatch($page, $query, $entityManager, $request, $session);
     }
 
-    #[Route(path: "/admin/<?= $route; ?>/delete/{entity}", name: "admin_<?= $route; ?>_delete", methods: ['DELETE', 'POST'])]
+    #[Route(path: '/admin/<?= $route; ?>/delete/{entity}', name: 'admin_<?= $route; ?>_delete', methods: ['DELETE', 'POST'])]
+    #[IsGranted('delete', 'entity')]
     public function delete(Entity $entity, EntityManager $entityManager, Request $request): Response
     {
         return $this->doDelete($entity, $entityManager, $request);
@@ -74,7 +81,11 @@ class <?= $class_name; ?> extends CrudController
 
     protected function getConfiguration(): CrudConfiguration
     {
-        return CrudConfiguration::create()
+        if ($this->configuration) {
+            return $this->configuration;
+        }
+
+        return $this->configuration = CrudConfiguration::create()
             ->setPageTitle('index', 'List of <?= $entity; ?>')
             ->setPageTitle('edit', 'Edition of {id}')
             ->setPageTitle('new', 'New <?= $entity; ?>')
