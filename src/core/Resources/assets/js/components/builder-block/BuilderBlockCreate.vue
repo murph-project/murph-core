89efd5475f4c7c1ee75875a9b120524ff5b73abf
Simon Vieille <simon@deblan.fr>
2024-05-26 21:42:31 +0200

    builder block: add search

--- src/core/Resources/assets/js/components/builder-block/BuilderBlockCreate.vue
+++ src/core/Resources/assets/js/components/builder-block/BuilderBlockCreate.vue
@@ -82,7 +82,7 @@
           <div
             v-for="(widget, name) in category.widgets"
             v-on:click="add(name, widget)"
-            v-if="matchSearch(name)"
+            v-if="matchSearch(label) || matchSearch(name)"
             class="widget col-auto"
           >
             <span class="widget-icon" v-if="widget.icon" v-html="widget.icon"></span>
