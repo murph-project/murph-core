b61cdbf32258155dd4c99c9b4148caaadbfcc4e0
Simon Vieille <simon@deblan.fr>
2025-05-19 12:01:50 +0200

    feat(collection): add delete_attr, add_attr options

--- src/core/Form/Type/CollectionType.php
+++ src/core/Form/Type/CollectionType.php
@@ -16,12 +16,23 @@ class CollectionType extends BaseCollectionType
     {
         parent::buildView($view, $form, $options);
 
+        $classes = [
+            'add_attr' => 'collection-add',
+            'delete_attr' => 'text-right',
+        ];
+
+        foreach ($classes as $key => $class) {
+            $options[$key]['class'] = $class.' '.($options[$key]['class'] ?? '');
+        }
+
         $view->vars = array_replace($view->vars, [
             'collection_name' => $options['collection_name'],
             'label_add' => $options['label_add'],
             'label_delete' => $options['label_delete'],
             'allow_add' => $options['allow_add'],
             'allow_delete' => $options['allow_delete'],
+            'add_attr' => $options['add_attr'],
+            'delete_attr' => $options['delete_attr'],
             'template_before_item' => $options['template_before_item'],
             'template_after_item' => $options['template_after_item'],
         ]);
@@ -37,6 +48,8 @@ class CollectionType extends BaseCollectionType
             'label_delete' => 'Delete',
             'template_before_item' => null,
             'template_after_item' => null,
+            'add_attr' => [],
+            'delete_attr' => [],
         ]);
     }
 
